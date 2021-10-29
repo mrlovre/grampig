09fe74b6c260475630add21f716a570fc4743dd7
Lovre Mrčela <lovre.mrcela@fer.hr>
2021-10-29 20:03:59 +0200

    Finish alpha version

---
+++ gram.py
@@ -0,0 +1,97 @@
+import contextlib
+import re
+from pathlib import Path
+import os
+
+import yaml
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
+
+        for key, value in self.items():
+            if isinstance(value, dict):
+                self[key] = AttrDict(value)
+            if isinstance(value, list):
+                for i, item in enumerate(value):
+                    if isinstance(item, dict):
+                        self[key][i] = AttrDict(item)
+
+@contextlib.contextmanager
+def redirect_stdout(stream):
+    import sys
+
+    sys.stdout = stream
+    yield
+    sys.stdout = sys.__stdout__
+
+def markup(string):
+    string = re.sub(r"[_＿](.*?)[_＿]", r"\\e{\1}", string)
+    string = re.sub(r"[(（](.*?)[)）][\[「](.*?)[]」]", r"\\f{\1}{\2}", string)
+    string = re.sub(r"(.)[\[「](.*?)[]」]", r"\\f{\1}{\2}", string)
+    string = re.sub(r"⇒", r"$\\Rightarrow$", string)
+
+    return string
+
+with open("src/gram.yaml") as file:
+    data = AttrDict(yaml.safe_load(file))
+
+Path("tex").mkdir(parents=True, exist_ok=True)
+
+for key, e in data.items():
+    with open(f"tex/{key}.tex", "w") as file, redirect_stdout(file):
+        options = [e.tezina]
+
+        if e.bless in ["y", "Y", "yes", "Yes", "YES", "true", "True", "TRUE", "on", "On", "ON"]:
+            options += ["bless"]
+
+        options = str.join(",", options)
+        print(rf"\documentclass[{options}]{{grampig}}")
+        print()
+        superscript = None
+        rbr = e.get("rbr", None)
+
+        if rbr:
+            superscript = rf"\textsuperscript{{{rbr}}}"
+
+        print(rf"\title{{{e.kljuc}{superscript}}}")
+        print(rf"\pos{{{e.vrsta}}}")
+        print()
+
+        print(r"\begin{document}")
+        print(r"    \maketitle")
+        print(rf"    {markup(e.obj1)}\\")
+        print(rf"    {markup(e.obj2)}")
+        print(r"    \vspace{0.5em}")
+        print()
+
+        n_fields = len(re.split(r"([＋⇒])", e.tvorba[0].jap))
+        cols = "c" * n_fields
+        print(r"    \begin{table}")
+        print(r"        \label{tab:tvorba}")
+        print(r"        \centering")
+        print(rf"        \begin{{tabular}}{{@{{}}{cols}@{{}}}}")
+
+        for tvorba in e.tvorba:
+            chunks = re.split(r"([＋⇒])", tvorba.jap)
+            chunks = map(markup, chunks)
+            chunks = str.join(" & ", chunks) + r" \bh"
+            print(rf"            {chunks}")
+            chunks = re.split(r"(\+|(?:=>))", tvorba.hrv)
+            chunks = [chunk if chunk not in ["+", "=>"] else "" for chunk in chunks]
+            chunks = map(markup, chunks)
+            chunks = str.join(" & ", chunks) + r" \\"
+            print(rf"            {chunks}")
+        print(r"        \end{tabular}")
+        print(r"    \end{table}")
+        print()
+
+        print(r"    \begin{itemize}")
+
+        for primjer in e.primjeri:
+            print(rf"        \item {markup(primjer.jap)}\bh")
+            print(rf"        {markup(primjer.hrv)}")
+
+        print(r"    \end{itemize}")
+        print(r"\end{document}")
---
+++ pig.py
@@ -0,0 +1,101 @@
+import contextlib
+import os
+from pathlib import Path
+
+import yaml
+import re
+
+@contextlib.contextmanager
+def redirect_stdout(stream):
+    import sys
+
+    sys.stdout = stream
+    yield
+    sys.stdout = sys.__stdout__
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
+
+        for key, value in self.items():
+            if isinstance(value, dict):
+                self[key] = AttrDict(value)
+            if isinstance(value, list):
+                for i, item in enumerate(value):
+                    if isinstance(item, dict):
+                        self[key][i] = AttrDict(item)
+
+def furiganize(string):
+    string = re.sub(r"(.)「(.*?)」", r"\\f{\1}{\2}", string)
+    return string
+
+def parse_entry(entry):
+    kanji, *rest = re.split(r"\s*,\s*", entry)
+    kanji = furiganize(kanji)
+    kanji = rf"\e{{{kanji}}}"
+    return kanji, *rest
+
+with open("src/pig.yaml") as file:
+    data = AttrDict(yaml.safe_load(file))
+
+spaces = 4
+
+Path("tex").mkdir(parents=True, exist_ok=True)
+
+for key in data.keys():
+    filename = key.replace(" ", "_")
+    entry = data[key]
+
+    with open(f"tex/{filename}.tex", "w") as file, redirect_stdout(file):
+        print(rf"\renewcommand{{\backgroundcolor}}{{{entry.pozadina}}}")
+        print(rf"\renewcommand{{\foregroundcolor}}{{{entry.prednjina}}}")
+        print(rf"\renewcommand{{\accentcolor}}{{{entry.istaknuto}}}")
+
+        indent = 0
+        print(rf"\documentclass{{grampig}}")
+        print()
+
+        print(r"\begin{document}")
+        indent += 1
+        gap = " " * indent * spaces
+        print(gap + r"\begin{minipage}{\width}")
+        print(gap + r"\centering")
+
+        title = [entry.pridjev, entry.imenica, entry.glagol]
+        title = map(lambda x: re.split(r"\s*,\s*", x)[0], title)
+        # title = map(furiganize, title)
+        title = map(lambda x: re.sub(r"「.*?」", "", x), title)
+        title = str.join("・", title)
+
+        print(gap + rf"{{\Large {title}}}")
+        print(gap + r"\vspace{2em}")
+        print()
+
+        indent += 1
+        gap = " " * indent * spaces
+        print(gap + r"\begin{tabular}{@{}r@{\hspace{2em}}c@{\hspace{2em}}l@{}}")
+
+        indent += 1
+        gap = " " * indent * spaces
+
+        for key2 in ["pridjev", "imenica", "glagol"]:
+            kanji, znacenje, vrsta = parse_entry(entry[key2])
+            vrsta = rf"\textit{{{vrsta}}}"
+            row = str.join(" & ", [kanji, znacenje, vrsta])
+            row += r" \br"
+            print(gap + row)
+
+        print(gap + r"\multicolumn{3}{c}{\e{\textsc{GRATIS}}} \br")
+
+        kanji, znacenje, vrsta = parse_entry(entry["katakana"])
+        vrsta = rf"\textit{{{vrsta}}}"
+        row = str.join(" & ", [kanji, znacenje, vrsta])
+        row += r" \\"
+        print(gap + row)
+
+        print(gap + r"\end{tabular}")
+        indent -= 1
+        gap = " " * indent * spaces
+        print(gap + r"\end{minipage}")
+        print(r"\end{document}")
